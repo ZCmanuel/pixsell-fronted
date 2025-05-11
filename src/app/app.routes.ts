9ae9f53f0bdebe8b5eb4822cc7cbde55576f2e36
Manuel Zamorano Cardell <manuelzamoranodaw@gmail.com>
2025-05-11 12:54:28 +0200

    feat: rutas y page-componente de las paginas auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: 'auth',
+        loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES),
+    },
+    {
         path: '',
         loadChildren: () => import('./features/public/public.routes').then(m => m.PUBLIC_ROUTES),
+    },
+    {
+        path: '**',
+        redirectTo: '',
     }
 ];
