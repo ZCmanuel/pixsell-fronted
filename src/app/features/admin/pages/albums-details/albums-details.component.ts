67c92d3ea7bfc11a477f9fd46a1c6c8a1dbde9ad
Manuel Zamorano Cardell <manuelzamoranodaw@gmail.com>
2025-05-11 15:09:57 +0200

    feat(routes): mejora de rutas client/admin con data para navbar

---
+++ src/app/features/admin/pages/albums-details/albums-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-albums-details',
+  imports: [],
+  templateUrl: './albums-details.component.html',
+})
+export class AlbumsDetailsComponent { }
---
+++ src/app/features/clients/pages/albums/albums.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-albums',
+  imports: [],
+  templateUrl: './albums.component.html',
+})
+export class AlbumsComponent { }
