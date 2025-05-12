d22e2b4299bebc33baf5a5292aa22c8db8a28f6a
Manuel Zamorano Cardell <manuelzamoranodaw@gmail.com>
2025-05-12 18:21:41 +0200

    feat: componentes sidear y head-component de usuarios

--- src/app/features/layouts/users-layout/users-layout.component.ts
+++ src/app/features/layouts/users-layout/users-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "../../../shared/components/user-app/sidebar/sidebar.component";
+import { HeaderComponent } from "../../../shared/components/user-app/header/header.component";
 
 @Component({
   selector: 'app-users-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
   templateUrl: './users-layout.component.html',
 })
 export class UsersLayoutComponent { }
--- src/app/shared/components/user-app/sidebar/sidebar.component.ts
+++ src/app/shared/components/user-app/sidebar/sidebar.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ADMIN_ROUTES } from '../../../../features/admin/admin.routes';
+import { CLIENT_ROUTES } from '../../../../features/clients/clients.routes';
 
 @Component({
-  selector: 'app-sidebar',
+  selector: 'sidebar',
   imports: [],
   templateUrl: './sidebar.component.html',
 })
-export class SidebarComponent { }
+export class SidebarComponent {
+  private router = inject(Router);
+
+  // Definimos la ruta de admin
+  private isAdmin = computed(() => this.router.url.startsWith('/admin'));
+
+  // Obtenemos las rutas visibles dinámicamente según contexto
+  readonly visibleRoutes = computed(() => {
+    const routes = this.router.url.startsWith('/admin') ? ADMIN_ROUTES : CLIENT_ROUTES
+    
+  });
+
+  logout() {
+    // Aquí puedes colocar tu lógica de logout
+    console.log('Cerrar sesión');
+  }
+}
